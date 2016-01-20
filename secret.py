8f0346384c1bcb0427e0e59245eb736b6685d8cf
Ian <surouxx@gmail.com>
2016-01-20 08:41:58 -0500

    Delete secret.py

--- secret.py
+++
@@ -1,5 +0,0 @@
-fromaddress = "seversonin@gilbertschool.org"
-toaddress = "surouxx@gmail.com"
-smptserver = "smtp.gmail.com"
-smptlogusrname = "seversonian@gmail.com"
-smptlogpasswd = "Nobodyshero12"
